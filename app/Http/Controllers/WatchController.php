12de340cf669911e10f9cf1b9b2e5dad8e4ef98a
Hải Lê <dark.hades.1102@gmail.com>
2017-04-15 20:53:14 +0900

    Route for watch

--- app/Http/Controllers/WatchController.php
+++ app/Http/Controllers/WatchController.php
@@ -3,11 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Film;
+use App\Episode;
 
 class WatchController extends Controller
 {
   public function index()
   {
     # code...
+  }
+
+  public function watch($film_id)
+  {
+    return 'film';
+  }
+
+  public function watchEpisode($film_id, $episode_id)
+  {
+    return 'ep';
   }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,12 +20,21 @@ Auth::routes();
 Route::get('/home', 'HomeController@index');
 
 Route::group(['prefix' => 'watch'], function() {
-  Route::get('/', 'WatchController@index')->name('watch');
+  // Route::get('/', 'WatchController@index')->name('watch');
 
-  Route::group(['prefix' => '{id}'], function($id) {
-    Route::get('/', function() use ($id) {
-      print_r($id);
-      return 1;
-    });
-  });
+  Route::get('/{film_id}/{film_slug?}', 'WatchController@watch');
+
+  Route::get('/{film_id}/{episode_id}/{film_slug?}/{episode_slug?}', 'WatchController@watchEpisode');
+
+  // Route::group(['prefix' => '{film_id}'], function($film_id) {
+  //   Route::get('/', function() use ($film_id) {
+  //     print_r($film_id);
+  //     return 1;
+  //   });
+  //
+  //   Route::get('/{eposide_id}', function($eposide_id) use ($film_id) {
+  //     print_r($id);
+  //     return 1;
+  //   });
+  // });
 });
