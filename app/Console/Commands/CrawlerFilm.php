5bde8fc5ee942e89d3b8a3038973918e458317ea
dark.hades.1102 <dark.hades.1102@gmail.com>
2017-04-18 05:09:05 -0400

    VuigheNet get link

--- app/Console/Commands/CrawlerFilm.php
+++ app/Console/Commands/CrawlerFilm.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use GuzzleHttp\Client;
+use App\Film;
 
 class CrawlerFilm extends Command
 {
@@ -21,6 +23,7 @@ class CrawlerFilm extends Command
     protected $description = 'Crawler link film from another website';
 
     protected $listDomain;
+    private $pickDomain;
   
     protected $results;
   
@@ -37,10 +40,10 @@ class CrawlerFilm extends Command
         'vuighe.net', //'anime47.com'
       ];
       
-      $this->results = [[
+      $this->results = [
         'name' => null,
         'description' => null,
-      ]];
+      ];
     }
 
     /**
@@ -50,16 +53,60 @@ class CrawlerFilm extends Command
      */
     public function handle()
     {
-      $domain = rand(0, count($this->listDomain)-1);
-      $funcName = studly_case((preg_replace('/[^A-Za-z0-9\-]/', '_', $this->listDomain[$domain])));
+      $this->pickDomain = rand(0, count($this->listDomain)-1);
+      $funcName = studly_case((preg_replace('/[^A-Za-z0-9\-]/', '_', $this->listDomain[$this->pickDomain])));
       if(method_exists($this, $funcName))
       {
+        $this->line("{$this->listDomain[$this->pickDomain]} is being crawler.");
         $this->{$funcName}();
       }
     }
   
     function VuigheNet()
     {
-      print_r("sldkf");
+      $client = new Client([
+        'base_uri' => 'http://'.$this->listDomain[$this->pickDomain],
+        'http_errors' => false,
+        'allow_redirects' => false,
+        'headers' => [
+          'X-Requested-With' => 'XMLHttpRequest',
+          'Referer'          => $this->listDomain[$this->pickDomain],
+        ],
+      ]);
+      
+      $limit = 0;
+      $offset = 0;
+      
+      while (true)
+      {
+        $url = "/api/v2/films?limit={$limit}&offset={$offset}";
+        $res = $client->request('GET', $url, []);
+        if ($res->getStatusCode() === 200)
+        {
+          $data = json_decode($res->getBody(), true);
+          if (isset($data['data']))
+          {
+            if ($offset === 0)
+            {
+              $bar = $this->output->createProgressBar($data['total']);
+              //$bar->setFormat('verbose');
+              $limit = 40;
+            }
+            if (isset($bar) === true)
+            {
+              $bar->advance(count($data['data']));
+              $offset += count($data['data']);
+              
+              if ($offset >= $bar->getMaxSteps())
+              {
+                break;
+              }
+            }
+          }
+        }
+      }
+      $bar->finish();
+      $this->line("{$bar->getMaxSteps()} is crawled.");
+      
     }
 }
