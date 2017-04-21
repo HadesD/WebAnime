c7e5574fd0088f814b4c371ad9071be74f6110c8
Hải Lê <dark.hades.1102@gmail.com>
2017-04-22 04:37:05 +0900

    Module video

--- resources/views/watch.blade.php
+++ resources/views/watch.blade.php
@@ -1,10 +1,21 @@
 @extends('layouts.app.home')
 @section('title', "{$episode->name} - {$film->name}")
+@push('css')
+  <link href="{{ asset('video.js/dist/video-js.css') }}" rel="stylesheet" />
+  <script type="text/javascript" src="{{ asset('video.js/dist/ie8/videojs-ie8.min.js') }}"></script>
+@endpush
 @section('content')
   <!-- Player video and list film -->
   <div class="ui internally stackable grid">
     <div class="twelve wide column">
-      sdfsdf
+      <video id="player-video" class="video-js" controls preload="auto" width="640" height="264" poster="MY_VIDEO_POSTER.jpg" data-setup="{}">
+        <source src="MY_VIDEO.mp4" type="video/mp4" />
+        <source src="MY_VIDEO.webm" type="video/webm" />
+        <p class="vjs-no-js">
+          To view this video please enable JavaScript, and consider upgrading to a web browser that
+          <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
+        </p>
+      </video>
     </div>
     <div class="four wide column">
       {{-- <div class="ui pointing secondary stackable menu">
@@ -23,3 +34,6 @@
     </div>
   </div>
 @endsection
+@push('js')
+  <script type="text/javascript" src="{{ asset('video.js/dist/video.min.js') }}"></script>
+@endpush
