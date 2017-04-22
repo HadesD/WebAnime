c8a8a07e3a6fc3c17305c133ef36473c028c29e4
Hải Lê <dark.hades.1102@gmail.com>
2017-04-22 20:59:51 +0900

    Size for player

--- resources/views/watch.blade.php
+++ resources/views/watch.blade.php
@@ -2,13 +2,44 @@
 @section('title', "{$episode->name} - {$film->name}")
 @push('css')
   <link href="{{ asset('video.js/dist/video-js.css') }}" rel="stylesheet" />
+  <style type="text/css">
+  #player-video {
+    width: 100%;
+    height: 460px;
+  }
+  @media (max-width: 767px) and (min-width: 640px) {
+    #player-video {
+      float: none;
+      /*width: 640px;*/
+      height: 360px;
+    }
+  }
+  @media (max-width: 639px) and (min-width: 480px) {
+    #player-video {
+        /*width: 480px;*/
+        height: 270px;
+    }
+  }
+  @media (max-width: 639px) and (min-width: 375px) {
+    #player-video {
+        /*width: 375px;*/
+        height: 211px;
+    }
+  }
+  @media (max-width: 639px) {
+    #player-video {
+      /*width: 320px;*/
+      height: 180px;
+    }
+  }
+  </style>
   <script type="text/javascript" src="{{ asset('video.js/dist/ie8/videojs-ie8.min.js') }}"></script>
 @endpush
 @section('content')
   <!-- Player video and list film -->
   <div class="ui internally stackable grid">
     <div class="twelve wide column">
-      <video id="player-video" class="video-js" controls preload="auto" width="640" height="264" poster="MY_VIDEO_POSTER.jpg" data-setup="{}">
+      <video id="player-video" class="video-js" controls preload="auto" poster="MY_VIDEO_POSTER.jpg" data-setup="{}">
         <source src="MY_VIDEO.mp4" type="video/mp4" />
         <source src="MY_VIDEO.webm" type="video/webm" />
         <p class="vjs-no-js">
@@ -31,6 +62,32 @@
       <div class="ui tab segment" data-tab="third">
         Third
       </div> --}}
+      <div class="ui very relaxed items">
+        <div class="item">
+          <div class="ui tiny image">
+            <img src="/images/wireframe/image.png">
+          </div>
+          <div class="middle aligned content">
+            <a class="header">12 Years a Slave</a>
+          </div>
+        </div>
+        <div class="item">
+          <div class="ui tiny image">
+            <img src="/images/wireframe/image.png">
+          </div>
+          <div class="middle aligned content">
+            <a class="header">My Neighbor Totoro</a>
+          </div>
+        </div>
+        <div class="item">
+          <div class="ui tiny image">
+            <img src="/images/wireframe/image.png">
+          </div>
+          <div class="middle aligned content">
+            <a class="header">Watchmen</a>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 @endsection
