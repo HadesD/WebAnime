23f9bafb768604c13fa14d14a0984090dcdecdd6
dark.hades.1102 <dark.hades.1102@gmail.com>
2017-04-26 20:24:26 -0400

    Routes fix]

--- routes/web.php
+++ routes/web.php
@@ -24,9 +24,9 @@ Route::group(['prefix' => 'dashboard', 'as' => 'dashboard.'], function () {
 Route::group(['prefix' => 'watch', 'as' => 'watch.'], function() {
   Route::get('/', 'WatchController@index')->name('index');
 
-  Route::get('/{film_id}/{film_slug?}', 'WatchController@watchFilm')->name('film');
+  Route::get('/{film_id}/{film_slug?}', 'WatchController@watchFilm')->where(['film_id' => '[0-9]+',])->name('film');
 
-  Route::get('/{film_id}/{episode_id}/{film_slug?}/{episode_slug?}', 'WatchController@watchEpisode')->name('episode');
+  Route::get('/{film_id}/{episode_id}/{film_slug?}/{episode_slug?}', 'WatchController@watchEpisode')->where(['film_id' => '[0-9]+', 'episode_id' => '[0-9]+',])->name('episode');
 
   // Route::group(['prefix' => '{film_id}'], function($film_id) {
   //   Route::get('/', function() use ($film_id) {
