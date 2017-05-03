9a4f33a4c559681ad6170b9f345cc20bcc903b4c
Hải Lê <dark.hades.1102@gmail.com>
2017-05-04 02:36:28 +0900

    Index

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,29 +19,45 @@
     margin-top: -28px;
     margin-left: -33px;
     cursor: pointer;
-    opacity: 0;
+    display: none;
+    z-index: 1001;
   }
-  #owl-demo .owl-item:hover i.icon{
-    opacity: 1;
+  #owl-demo .mask{
+    position: absolute;
+    z-index: 1000;
+    padding: 0 1em;
+    width: 100%;
+    height: 50px!important;
+    bottom: 0;
+    left: 0;
+    background: #000;
+    background: -webkit-linear-gradient(transparent, #000); /* For Safari 5.1 to 6.0 */
+    background: -o-linear-gradient(transparent, #000); /* For Opera 11.1 to 12.0 */
+    background: -moz-linear-gradient(transparent, #000); /* For Firefox 3.6 to 15 */
+    background: linear-gradient(transparent, #000); /* Standard syntax */
   }
   </style>
 @endpush
 @section('content')
-  <div class="ui segment">
+  <div class="ui inverted segment">
     <div id="owl-demo" class="owl-carousel owl-theme">
-    @foreach ($carousel as $film)
-      <a href="{{ $film->getRoute() }}">
-        <div class="item">
-            <img src="{{ $film->thumbnail }}" />
+      @foreach ($carousel as $film)
+        <a href="{{ $film->getRoute() }}">
+          <div class="item">
+            <div class="ui inverted dimmer"></div>
+            <div class="mask">
+              <h4 class="ui inverted header">{{ $film->name }}</h4>
+            </div>
+            <img class="ui medium bordered image" src="{{ $film->thumbnail }}" />
             <i class="video play huge icon"></i>
-        </div>
-      </a>
-    @endforeach
+          </div>
+        </a>
+      @endforeach
+    </div>
   </div>
+  <div class="ui dividing red header">
+    @lang('watch.lastest')
   </div>
-  <h3 class="ui dividing red header">
-    Dividing Header
-  </h3>
   <div class="ui five special stackable cards">
     @foreach ($newestList as $newest)
       <div class="ui red card">
@@ -92,13 +108,27 @@
     $('.special.cards .image').dimmer({
       on: 'hover'
     });
-    $(document).ready(function() {
+    $(function() {
       $("#owl-demo").owlCarousel({
-        autoPlay : 3000,
+        autoplay: true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:false,
         loop: true,
-        margin:10,
-        autoWidth:true,
+        margin: 10,
+        autoWidth: true,
       });
+      $(document).on({
+        mouseenter: function () {
+          $(this).find(".item").dimmer('show');
+          $(this).find("i.icon").fadeIn("fast");
+          console.log(1);
+        },
+        mouseleave: function () {
+          $(this).find(".item").dimmer('hide');
+          $(this).find("i.icon").fadeOut("fast");
+          console.log(2);
+        }
+      }, "#owl-demo .owl-item");
     });
   </script>
 @endpush
